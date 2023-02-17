8a69a591c70a0bc6b5c2ef266d8eb39d10771ceb
Chirag Dudhat <chiragdudhat28@gmail.com>
2023-02-17 16:28:30 +0100

    @chiragdudhat

---
+++ L1_T2_Pattern_CDudhat.cpp
@@ -0,0 +1,27 @@
+
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+	
+	clrscr();
+	printf("\t================================\n");
+	printf("\t\t     Pattern\t\n");
+	printf("\t================================\n");
+	printf("\n\t\t\tR");
+	printf("\n\t\t       E E");
+	printf("\n\t\t      D   D");
+	printf("\n\t\t     &     &");
+	printf("\n\t\t    W       W");
+	printf("\n\t\t     H     H");
+	printf("\n\t\t      I   I");
+	printf("\n\t\t       T T");
+	printf("\n\t\t\tE");
+	printf("\n\n\t================================\n");
+	getch();
+	
+}
+
+
